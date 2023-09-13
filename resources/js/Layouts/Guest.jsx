67575971e54cd6313fab1f1e1237ef6a7685a9cd
Bui_Danh_Tung <bui.danh.tung@sun-asterisk.com>
2023-09-13 15:45:15 +0700

    Setup register layout for guest
    update
    
    update

--- resources/js/Layouts/Guest.jsx
+++ resources/js/Layouts/Guest.jsx
@@ -11,7 +11,7 @@ export default function Guest({ children }) {
                 </Link>
             </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div className="w-full sm:max-w-6xl mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {children}
             </div>
         </div>
