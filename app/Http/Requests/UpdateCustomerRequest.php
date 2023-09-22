9f2d800fd2ec7b4a05aa816b8abff67206584d6d
dangvm-0369 <vu.minh.dang@sun-asterisk.com>
2023-09-22 10:11:00 +0700

    FE+BE Edit customer

---
+++ app/Http/Requests/UpdateCustomerRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'string|required|min:3|max:255',
+            'phone' => 'string|required|min:3|max:30',
+        ];
+    }
+}
